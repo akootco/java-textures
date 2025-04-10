87ba67aed03a77758603e846c4813be2a610b4f8
IndiaPaleAle <contact@maltsburg.com>
2025-04-09 22:34:41 -0700

    Merge packs (#15)
    * combine food pack with plushie pack
    - added particles files
    
    * combine food pack with plushie pack
    - added particles files
    
    * erm! some changes
    - improved party hat display
    - now using less items for food
     - most food is now baked potato or steak
     - crops, displays and other things will stay the same since they are used differently in the plugin
    - new paper update adds custom weapons, all custom weapons should be netherite swords
    - remove glass cup
    
    * delete CSV and update script
    
    * fix some things

--- zip.py
+++ zip.py
@@ -9,24 +9,18 @@ def get_icon():
 get_icon()
 
 # zip everything
-def zip_directory(src_dir, zip_name, exclude_folders=[], exclude_files=[]):
+def zip_directory(src_dir, zip_name):
     with zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED) as zipf:
         for root, dirs, files in os.walk(src_dir):
             # exclude the specified folders
-            dirs[:] = [d for d in dirs if d not in exclude_folders]
+            dirs[:] = [d for d in dirs if d not in ['.git', ".idea"] ]
 
             for file in files:
-                # skip the zip file being created
-                if file == zip_name:
-                    continue
-
-                if file in exclude_files:
+                # skip files
+                if file in {zip_name, 'zip.py'}:
                     continue
 
                 file_path = os.path.join(root, file)
                 zipf.write(file_path, os.path.relpath(file_path, src_dir))
 
-folders_to_exclude = ['.git', ".idea"]
-files_to_exclude = ['zip.py', 'zip.bat', 'items.csv']
-
-zip_directory('./', 'food_pack.zip', folders_to_exclude, files_to_exclude)
+zip_directory('./', 'food_pack.zip')
