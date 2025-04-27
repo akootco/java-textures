aac5fce3a1c044612a22f0f65820f5bbd5e3fc98
IndiaPaleAle <154647446+Maltsburg@users.noreply.github.com>
2025-04-27 12:14:33 -0700

    release workflow

---
+++ changes.md
@@ -0,0 +1,11 @@
+# Akoot&Co Resource Pack
+### Java 1.21.4 
+
+---
+
+**Created by:**
+
+- ![lilhashbrown](https://maltsburg.com/head/lilhashbrown/12) **lilhashbrown**
+- ![Bee143](https://maltsburg.com/head/Bee143/12) **[Bee143](https://github.com/Bee143)**
+- ![IndiaPaleAle](https://maltsburg.com/head/indiapaleale/12) **[IndiaPaleAle](https://github.com/Maltsburg)**
+- ![Akoot_](https://maltsburg.com/head/akoot_/12) **[Akoot_](https://github.com/akoot)**
--- zip.py
+++ zip.py
@@ -1,6 +1,5 @@
 import json
 import os
-import zipfile
 import requests
 # get server icon
 def get_icon():
@@ -23,21 +22,3 @@ def generate_sound_definitions(folder, output_file):
 
 # Call it (change folder path if needed)
 generate_sound_definitions("assets/plushies/sounds", "assets/plushies/sounds.json")
-
-
-# zip everything
-def zip_directory(src_dir, zip_name):
-    with zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED) as zipf:
-        for root, dirs, files in os.walk(src_dir):
-            # exclude the specified folders
-            dirs[:] = [d for d in dirs if d not in ['.git', ".idea"] ]
-
-            for file in files:
-                # skip files
-                if file in {zip_name, 'zip.py'}:
-                    continue
-
-                file_path = os.path.join(root, file)
-                zipf.write(file_path, os.path.relpath(file_path, src_dir))
-
-zip_directory('./', 'food_pack.zip')
