5bcf2696f882bc9482e1d25969e6273cff7f69f8
IndiaPaleAle <154647446+Maltsburg@users.noreply.github.com>
2026-05-04 02:49:32 -0700

    replace pack.png, delete old script

--- zip.py
+++
@@ -1,7 +0,0 @@
-import requests
-# get server icon
-def get_icon():
-    with open("pack.png", "wb") as f:
-        f.write(requests.get("https://api.mcstatus.io/v2/icon/akoot.co").content)
-
-get_icon()
